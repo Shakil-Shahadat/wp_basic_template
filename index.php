013f57379fb35c12bb4b65cb394189b66fb4afba
Shakil Shahadat <shakil.regi@gmail.com>
2017-07-01 07:40:17 +0600

    Saving Initial Snap

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Basic Wordpress Theme</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>Site Header</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<div class="wraper-for-main-aside">
+		<main>
+			<h2>Main Content</h2>
+			This is main content area.
+		</main>
+
+		<aside>
+			<h2>Sidebar</h2>
+			This is a sidebar area.
+		</aside>
+	</div>
+
+	<footer>
+		<h2>Footer Area</h2>
+		This is a footer area.
+	</footer>
+</body>
+</html>

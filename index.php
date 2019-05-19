461d1953cd5f8bd91d1138d44986ae4ada7af92d
Shakil Shahadat <shakil.regi@gmail.com>
2019-05-19 16:38:38 +0600

    Integrates bootstrap

--- index.php
+++ index.php
@@ -2,46 +2,69 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Basic Wordpress Theme</title>
-	<link rel="stylesheet" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Basic Theme</title>
+
+	<link rel="shortcut icon" href="src/favicon.ico">
+
+	<link rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="src/style.css">
 </head>
 <body>
 
 <div class="container">
 
-	<header>
+	<div class="row">
 
-		<h1>Header</h1>
-		<nav>
-			<ul>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Contact</a></li>
-			</ul>
-		</nav>
+		<header class="col">
 
-	</header>
+			<h1>Header</h1>
+			<nav>
+				<ul class="nav">
+					<li class="nav-item">
+						<a class="nav-link active" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contact</a>
+					</li>
+				</ul>
+			</nav>
 
-	<div class="wraper">
+		</header>
 
-		<main>
+	</div>
+
+	<div class="row">
+
+		<main class="col-9">
 			<h2>Main Content</h2>
-			This is the main content area.
+			Main content area.
 		</main>
 
-		<aside>
+		<aside class="col">
 			<h2>Sidebar</h2>
-			This is the sidebar area.
+			Sidebar area.
 		</aside>
 
-	</div> <!-- .wraper END -->
+	</div> <!-- .row END -->
 
-	<footer>
-		<h2>Footer</h2>
-		This is the footer area.
-	</footer>
+	<div class="row">
+
+		<footer class="col">
+			<h2>Footer</h2>
+			Footer area.
+		</footer>
+
+	</div>
 
 </div> <!-- .container END -->
 
+
+<script src="src/jquery-3.4.1.slim.min.js"></script>
+<script src="src/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
